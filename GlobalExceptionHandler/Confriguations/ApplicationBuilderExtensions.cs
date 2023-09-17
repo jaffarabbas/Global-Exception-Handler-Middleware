07737d39d204ad79867d8b3df08cb972c43b2133
jaffarabbas <gamaportal8@gmail.com>
2023-09-17 22:28:38 +0500

    Initial working for MIddleware

---
+++ GlobalExceptionHandler/Confriguations/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+
+namespace GlobalErrorHandling.Confriguations
+{
+    public static class ApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Custom Error Handler Middleware
+        /// </summary>
+        /// <param name="applicationBuilder"></param>
+        /// <returns></returns>
+        public static IApplicationBuilder UseGlobalErrorHandler(this IApplicationBuilder applicationBuilder)
+            => applicationBuilder.UseMiddleware<GlobalExceptionsHandlingMiddleware>();
+    }
+}
---
+++ GlobalExceptionHandler/Confriguations/GlobalExceptionsHandlingMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using GlobalErrorHandling.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.Json;
+using KeyNotFoundException = GlobalErrorHandling.Exceptions.KeyNotFoundException;
+using NotImplementedException = GlobalErrorHandling.Exceptions.NotImplementedException;
+using UnAuthorizedAccessException = GlobalErrorHandling.Exceptions.UnAuthorizedAccessException;
+
+namespace GlobalErrorHandling.Confriguations
+{
+    public class GlobalExceptionsHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public GlobalExceptionsHandlingMiddleware(RequestDelegate request)
+        {
+            _next = request;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context,ex);
+            }
+        }
+
+        /// <summary>
+        /// Custom Error Handler
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="ex"></param>
+        /// <returns></returns>
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var status = HttpStatusCode.InternalServerError;
+            var message = ex.Message;
+            var stackTrace = ex.StackTrace;
+
+            switch (ex)
+            {
+                case NotFoundException:
+                    status = HttpStatusCode.NotFound;
+                    break;
+                case KeyNotFoundException:
+                    status = HttpStatusCode.NotFound;
+                    break;
+                case BadReuestException:
+                    status = HttpStatusCode.BadRequest;
+                    break;
+                case NotImplementedException:
+                    status = HttpStatusCode.NotImplemented;
+                    break;
+                case UnAuthorizedAccessException:
+                    status = HttpStatusCode.Unauthorized;
+                    break;
+            }
+
+            var exceptionResult = JsonSerializer.Serialize(new { error = message, stackTrace });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)status;
+            return context.Response.WriteAsync(exceptionResult);
+        }
+
+    }
+}
